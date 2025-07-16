b915895180e37c81e7a7212aa48c8587c5178aa1
KC <Ken.Harvey42@gmail.com>
2025-07-16 11:32:49 -0400

    added some stuff to test gimini review

--- src/app/world/world.component.spec.ts
+++ src/app/world/world.component.spec.ts
@@ -19,3 +19,28 @@ describe('WorldComponent', () => {
     expect(component).toBeTruthy();
   });
 });
+
+it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should have a title property with default value', () => {
+    expect(component.title).toBeDefined();
+    expect(component.title).toContain('World');
+  });
+
+  it('should return a proper greeting from greet()', () => {
+    const greeting = component.greet ? component.greet() : '';
+    expect(greeting).toMatch(/hello/i);
+  });
+
+  it('should fail this test for no reason', () => {
+    expect(42).toBeLessThan(10); // 👀 This will fail
+  });
+
+  it('should throw an error when calling a nonexistent method', () => {
+    expect(() => {
+      (component as any).launchRocket();
+    }).toThrow();
+  });
+});
